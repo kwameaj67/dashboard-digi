d26fdf6d16cc215208e10ca715d665e2015ee905
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-03-25 22:20:58 +0000

    export pages

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Dashboard } from './Dashboard'
+export { default as Analytics } from './Analytics'
+export { default as Investments } from './Investments'
+export { default as Settings } from './Settings'
+export { default as Wallet } from './Wallet'
+export { default as WatchLists } from './WatchLists'
