b51ba0e24f08e394c77153e9369949ac681404f8
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-03-25 22:22:28 +0000

    modified app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import SideBar from './components/SideBar/sideBar'
+import Page from './components/MainArea/page'
+import { BrowserRouter as Router, } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <SideBar/>
+          <Page/>
+       {/* <div className="sidebar">
+       </div> */}
+       {/* <div className="area"> 
+       </div> */}
+        </Router>
     </div>
   );
 }
