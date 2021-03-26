ee9afe6ffb45a31556aaa9a7ee3a99ceea5a98b6
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-03-26 00:58:12 +0000

    modify page section

--- src/components/MainArea/page.js
+++ src/components/MainArea/page.js
@@ -2,24 +2,37 @@ import React from 'react'
 import {
     Switch,
     Route,
+    useLocation
   } from "react-router-dom";
 import routes from '../SideBar/sideBarData'
 import "./page.css"
 // import { Dashboard,Investments,Analytics,Settings,Wallet,WatchLists } from '../../pages/index'
+import {
+    CSSTransition
+  } from "react-transition-group";
 
 const Page = () => {
+    const location = useLocation()
     return (
-        <div class="pageArea">
-            <Switch>
-                {routes.map((route, index) => (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        children={<route.main />}
-                    />
-                ))}
-            </Switch>
+        <div className="pageArea">
+                <div className="pageContent">
+                    <CSSTransition
+                        key={location.key}
+                        classNames="fade"
+                        timeout={300}
+                        >
+                        <Switch location={location}>
+                            {routes.map((route, index) => (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    children={<route.main />}
+                                />
+                            ))}
+                        </Switch>
+                        </CSSTransition>
+                </div>
         </div>
     )
 }
