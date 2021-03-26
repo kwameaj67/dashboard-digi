80cd4c35f112bb5d302065c77f39c03cff439853
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-03-26 00:58:25 +0000

    change sidebar icon

--- src/components/SideBar/sideBarData.js
+++ src/components/SideBar/sideBarData.js
@@ -1,7 +1,7 @@
 import { Dashboard,Investments,Analytics,Settings,Wallet,WatchLists } from '../../pages/index'
 import {MdDashboard} from 'react-icons/md'
 import {RiMoneyCnyCircleLine} from 'react-icons/ri'
-import {BiWallet} from 'react-icons/bi'
+import {SiCashapp} from 'react-icons/si'
 import {VscListSelection} from 'react-icons/vsc'
 import {SiGoogleanalytics} from 'react-icons/si'
 import {FiSettings} from 'react-icons/fi'
@@ -11,42 +11,42 @@ const routes = [
       path: "/dashboard",
       exact: true,
       title:"Dashboard",
-      icon:<MdDashboard/>,
+      icon:<MdDashboard size={24}/>,
       main: () => <Dashboard/>
     },
     {
       path: "/investments",
       exact: true,
       title:"Investments",
-      icon:<RiMoneyCnyCircleLine/>,
+      icon:<RiMoneyCnyCircleLine size={24}/>,
       main: () => <Investments/>
     },
     {
       path: "/wallet",
       exact: true,
       title:"Wallet",
-      icon:<BiWallet/>,
+      icon:<SiCashapp  size={24}/>,
       main: () => <Wallet/>
     },
     {
       path: "/watchlists",
       exact: true,
       title:"WatchLists",
-      icon:<VscListSelection/>,
+      icon:<VscListSelection size={24}/>,
       main: () => <WatchLists/>
     },
     {
       path: "/analytics",
       exact: true,
       title:"Analytics",
-      icon:<SiGoogleanalytics/>,
+      icon:<SiGoogleanalytics size={24}/>,
       main: () => <Analytics/>
     },
     {
       path: "/settings",
-      exact: true,
+    //   exact: true,
       title:"Settings",
-      icon:<FiSettings/>,
+      icon:<FiSettings size={24}/>,
       main: () => <Settings/>
     },
     
