e3c024bb20d3b199d3dbb4a611c8fa5de69f6e17
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-03-27 00:14:09 +0000

    changes to pageTitle

--- src/components/PageTitle/pageTitle.js
+++ src/components/PageTitle/pageTitle.js
@@ -2,11 +2,23 @@ import React from 'react'
 import './pageTitle.css'
 import {Switch,Route} from 'react-router-dom'
 import routes from '../SideBar/sideBarData'
+import {FaUserAlt} from 'react-icons/fa'
+import {CgMenuLeft} from 'react-icons/cg'
 
 
+// const  openNav = () => {
+//     // document.getElementById("sideBarArea").style.width = "280px";
+//     let sidebar = document.querySelector('.sideBarArea')
+//     sidebar.classList.toggle('active')
+//   }
+  
+  
 const PageTitle = () => {
     return(
           <div className="pageTitle">
+                <div className="menu"  >
+                    <CgMenuLeft size={30}/>
+                </div>
                     {
                     <Switch>
                         {routes.map((route, index) => (
@@ -19,6 +31,9 @@ const PageTitle = () => {
                         ))}
                     </Switch>
                     }
+                    <div className="profile">
+                            <FaUserAlt className="profile-icon" size={15} color="#f4f7fa"/>
+                    </div>
                 </div> 
     )
 } 
